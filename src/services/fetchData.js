c03dfb755b9a04ed833caf1923ff6d4518c894e6
Sxela <defiler.off@gmail.com>
2018-12-17 08:21:02 +0300

    Added automatical outgoiing links fetch and display for child nodes on parent click Incoming ETH and txes display, color indication of nested children array

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -2,5 +2,8 @@ import api from '@/services/api'
 export default {
   fetchData (params) {
     return api().get(`addresses/${params}`)
+  },
+  fetchData (prefix, params) {
+    return api().get(`addresses/${prefix}/${params}`)
   }
 }
