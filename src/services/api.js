0f344f3b939c9d440027a9cf4639e187c6268d12
Sxela <defiler.off@gmail.com>
2019-01-27 12:27:29 +0300

    add https

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export default () => {
   return axios.create({
   //  baseURL: 'http://localhost:8081'
-  baseURL: 'http://api.nodel.ink:8081'
+  baseURL: 'https://api.nodel.ink:8081'
   //baseURL: 'http://95.220.165.203:8081'
   //baseURL: 'http://sxela.asuscomm.com:8081'
   })
